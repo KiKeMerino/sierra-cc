6a21872c47d360fb5e57bdd25a51ffec23db0e7c
Kike Merino <palavix@gmail.com>
2025-03-11 13:00:07 +0100

    Dataset recortado al area de la cuenca

--- transformacion_datos.py
+++ transformacion_datos.py
@@ -9,15 +9,11 @@ modis_path = "./data/ejemplo_adda.hdf"
 area_path = "data/cuencas/adda-bornio/Adda-Bormio_basin.shp"
 snow_cover = open_bands_boundary(modis_path, area_path)
 
-nieve = (snow_cover["CGF_NDSI_Snow_Cover"].data > 40) & (snow_cover["CGF_NDSI_Snow_Cover"].data < 100)
-print(type(nieve))
 df = pd.DataFrame(snow_cover["CGF_NDSI_Snow_Cover"])
-nieve = (df > 40) & (df < 100) 
-print(type(nieve))
+nieve = (df >= 40) & (df <= 100) 
 
 df = np.where(nieve, 1, 0)
 
-
-print(df)
+print(df.tolist())
 print(f"Numero de ceros: {(df == 0).sum()}")
 print(f"Numero de unos: {(df == 1).sum()}")
