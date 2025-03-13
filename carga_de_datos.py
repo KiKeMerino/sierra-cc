4dc656be7f921c1003b93c04c5e855329b6780e9
Kike <palavix@gmail.com>
2025-03-13 13:42:54 +0100

    Primera version para leer datos del disco externo y contar los ceros y unos

--- carga_de_datos.py
+++ carga_de_datos.py
@@ -66,21 +66,21 @@ def open_bands_boundary(modis_path, area_path, variable="CGF_NDSI_Snow_Cover"):
     return band
 
 
-snow_cover = open_bands_boundary(modis_path, area_path)
+# snow_cover = open_bands_boundary(modis_path, area_path)
 
-# Obtener latitud y longitud del DataArray
-latitudes = snow_cover.y.values
-longitudes = snow_cover.x.values
+# # Obtener latitud y longitud del DataArray
+# latitudes = snow_cover.y.values
+# longitudes = snow_cover.x.values
 
-modis_ext = plotting_extent(snow_cover.to_array().values[0],
-                            snow_cover.rio.transform())
-f, ax = plt.subplots()
-ep.plot_bands(  snow_cover.to_array().values[0],
-                ax=ax,
-                extent=modis_ext,
-                title="Cubierta de nieve")
-# area = gpd.read_file(area_path)
-# area.plot( ax=ax,
-#           color=(1, 0, 0, 0.2)) # RGB - transparencia
+# modis_ext = plotting_extent(snow_cover.to_array().values[0],
+#                             snow_cover.rio.transform())
+# f, ax = plt.subplots()
+# ep.plot_bands(  snow_cover.to_array().values[0],
+#                 ax=ax,
+#                 extent=modis_ext,
+#                 title="Cubierta de nieve")
+# # area = gpd.read_file(area_path)
+# # area.plot( ax=ax,
+# #           color=(1, 0, 0, 0.2)) # RGB - transparencia
 
-plt.show()
+# plt.show()
--- transformacion_datos.py
+++ transformacion_datos.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,15 +6,32 @@ import rioxarray
 import rasterio as rio
 from carga_de_datos import open_bands_boundary
 
-modis_path = "./data/ejemplo_adda.hdf"
-area_path = "data/cuencas/adda-bornio/Adda-Bormio_basin.shp"
-snow_cover = open_bands_boundary(modis_path, area_path)
 
-df = pd.DataFrame(snow_cover["CGF_NDSI_Snow_Cover"])
-nieve = (df >= 40) & (df <= 100) 
+external_disk = "E:\\"
+area_path = external_disk + "data/cuencas/adda-bornio/Adda-Bormio_basin.shp"
+ruta_adda_bornio = external_disk + "data"
+print(area_path)
+# Lectura de datos
+try:
+    ficheros_hdf = os.listdir(ruta_adda_bornio)
+    ficheros_hdf = [os.path.join(ruta_adda_bornio, fichero) for fichero in ficheros_hdf if fichero.endswith('.hdf') and os.path.isfile(os.path.join(ruta_adda_bornio, fichero))]
 
-df = np.where(nieve, 1, 0)
+except FileNotFoundError:
+    print(f"El directorio '{ruta_adda_bornio}' no fue encontrado.")
 
-print(df.tolist())
-print(f"Numero de ceros: {(df == 0).sum()}")
-print(f"Numero de unos: {(df == 1).sum()}")
+# Función para mapear todos los valores entre 40 y 100 (inclusive) a:
+    # 0: no hay nieve
+    # 1: hay nieve
+def snow_mapping(df):
+    nieve = (df >= 40) & (df <= 100)
+    result = np.where(nieve, 1, 0)
+    return result
+
+
+for ruta in ficheros_hdf:
+    snow_cover = open_bands_boundary(ruta, area_path)
+    dataset = pd.DataFrame(snow_cover["CGF_NDSI_Snow_Cover"])
+    df = snow_mapping(dataset)
+
+    print(f"Numero de ceros: {(df == 0).sum()}")
+    print(f"Numero de unos: {(df == 1).sum()}")
