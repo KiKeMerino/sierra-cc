f5c1a217396d1c2686f3c66edc9a396fb3cc31b5
Kike Merino <palavix@gmail.com>
2025-03-24 10:42:59 +0100

    Update graficos y elimino carga_de_datos.py

--- graficos.py
+++ graficos.py
@@ -1,21 +1,47 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from obtencion_datos import obtener_datos
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import earthpy.plot as ep
+import os
+from rasterio.plot import plotting_extent
+import rioxarray as rxr
 
 # Obtener los datos
-datos = obtener_datos("data/cuencas/machopo-almendros/231750394/MOD10A1F_A2015001_h12v12_061_2021316063557_HEGOUT.hdf")
-datos = obtener_datos("data/ejemplo.hdf")
+archivo = "ejemplos/adda-bornio.hdf"
+area = gpd.read_file("D:/data/cuencas/adda-bornio/Adda-Bormio_basin.shp")
+snow_cover = rxr.open_rasterio(archivo, masked=True, variable="CGF_NDSI_Snow_Cover").rio.clip(
+                    area.geometry.to_list(), crs=area.crs, all_touched=True).squeeze()
 
 # Crear un histograma de los datos de la variable "CGF_NDSI_Snow_Cover"
-plt.hist(datos["CGF_NDSI_Snow_Cover"].values.flatten(), bins=50)
+plt.hist(snow_cover["CGF_NDSI_Snow_Cover"].values.flatten(), bins=50)
 plt.xlabel("Valor")
 plt.ylabel("Frecuencia")
 plt.title("Histograma de CGF_NDSI_Snow_Cover")
-plt.savefig("img/hist-CGF_NDSI_Snow_Cover")
+# plt.savefig("img/hist-CGF_NDSI_Snow_Cover")
 
 # Crear un mapa de calor de los datos de la variable "CGF_NDSI_Snow_Cover"
-sns.heatmap(datos["CGF_NDSI_Snow_Cover"])
+sns.heatmap(snow_cover["CGF_NDSI_Snow_Cover"])
 plt.xlabel("Columna")
 plt.ylabel("Fila")
 plt.title("Mapa de calor de CGF_NDSI_Snow_Cover")
-plt.savefig("img/heatmap-CGF_NDSI_Snow_Cover")
+# plt.savefig("img/heatmap-CGF_NDSI_Snow_Cover")
+
+
+
+# # Obtener latitud y longitud del DataArray
+latitudes = snow_cover.y.values
+longitudes = snow_cover.x.values
+
+modis_ext = plotting_extent(snow_cover.to_array().values[0],
+                            snow_cover.rio.transform())
+f, ax = plt.subplots()
+ep.plot_bands(  snow_cover.to_array().values[0],
+                ax=ax,
+                extent=modis_ext,
+                title="Cubierta de nieve")
+
+area.plot( ax=ax,
+          color=(1, 0, 0, 0.2)) # RGB - transparencia
+
+plt.show()
