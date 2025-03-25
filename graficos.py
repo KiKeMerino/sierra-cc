77c86824b0c566fbeb2701815baee8fb5045e798
Kike Merino <palavix@gmail.com>
2025-03-25 09:52:50 +0100

    Experimentos con graficos sns

--- graficos.py
+++ graficos.py
@@ -19,30 +19,37 @@ snow_cover = rxr.open_rasterio(archivo, masked=True, variable="CGF_NDSI_Snow_Cov
 # Reproyecto a WGS84
 snow_cover = snow_cover.rio.reproject("EPSG:4326")
 
+
+#%%
+data = snow_cover["CGF_NDSI_Snow_Cover"]
+datos = pd.read_csv("D:/data/adda-bornio.csv")
+
+#%%
+# Ordenar por fecha y cambiar formato
+datos.set_index('fecha', inplace=True)
+datos.index = pd.to_datetime(datos.index)
+datos.sort_index(inplace=True)
+datos.index = datos.index.strftime('%d/%m/%Y')
+
+#%%
+datos["no-nieve"] = datos["adda-bornio"].apply(lambda x: x[0])
+datos
+
+#%%
+datos.iloc[1]
+
 #%%
 # Crear un histograma de los datos de la variable "CGF_NDSI_Snow_Cover"
-plt.hist(snow_cover["CGF_NDSI_Snow_Cover"].values.flatten(), bins=50)
+plt.hist(data, bins=1)
 plt.xlabel("Valor")
 plt.ylabel("Frecuencia")
 plt.title("Histograma de CGF_NDSI_Snow_Cover")
 plt.show()
-plt.savefig("img/hist-CGF_NDSI_Snow_Cover")
+# plt.savefig("img/hist-CGF_NDSI_Snow_Cover")
 
 #%%
 # # Crear un mapa de calor de los datos de la variable "CGF_NDSI_Snow_Cover"
-sns.heatmap(snow_cover["CGF_NDSI_Snow_Cover"])
-plt.xlabel("Columna")
-plt.ylabel("Fila")
-plt.title("Mapa de calor de CGF_NDSI_Snow_Cover")
-plt.show()
-plt.savefig("img/heatmap-CGF_NDSI_Snow_Cover")
+heatmap = sns.heatmap(data, vmin=0, vmax=100)
+sns.set_color_codes(palette="dark")
+# plt.savefig("img/heatmap-CGF_NDSI_Snow_Cover")
 
-#%%
-cubierta = snow_cover["CGF_NDSI_Snow_Cover"]
-# # Crear una figura y ejes
-fig, ax = plt.subplots()
-#Mostrar snow_cover en los ejes.
-cubierta.plot(ax = ax)
-#Añadir un titulo
-ax.set_title("Cubierta de nieve")
-plt.show()
