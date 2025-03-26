5815bfdcac35195f3d07a51294de82e5c9b66cf4
Kike <palavix@gmail.com>
2025-03-26 22:36:37 +0100

    Funcion calcular area de nieve terminada y funcionamiento correcto

--- transformacion_datos.py
+++ transformacion_datos.py
@@ -7,99 +7,64 @@ import re
 import datetime
 import rioxarray as rxr
 
-external_disk = "D:/"
+external_disk = "E:/"
 data_path = os.path.join(external_disk, "data/", "cuencas/")
 
-# Función para mapear todos los valores entre 40 y 100 (inclusive) a:
-    # 0: no hay nieve
-    # 1: hay nieve
-def snow_mapping(array):
-    nieve = (array >= 40) & (array <= 100)
-    return np.where(nieve, 1, 0)
+def calcular_area_nieve(snow_cover, cuenca):
+    if cuenca == "adda-bornio":
+        snow_cover = snow_cover.rio.reproject("EPSG:25832")
+    elif cuenca == "genil-dilar":
+        snow_cover = snow_cover.rio.reproject("EPSG:25830")
+    elif cuenca == "indrawati-melamchi":
+        snow_cover = snow_cover.rio.reproject("EPSG:32645")
+    elif cuenca == "machopo-almendros":
+        snow_cover = snow_cover.rio.reproject("EPSG:32719")
+    elif cuenca == "nenskra-Enguri":
+        snow_cover = snow_cover.rio.reproject("EPSG:32638")
+    elif cuenca == "uncompahgre-ridgway":
+        snow_cover = snow_cover.rio.reproject("EPSG:32613")
 
-cuencas = os.listdir(data_path)
+    df = pd.DataFrame(snow_cover["CGF_NDSI_Snow_Cover"])
 
-df_adda_bornio = pd.DataFrame()
-df_genil_dilar = pd.DataFrame()
-df_indrawati_almendros = pd.DataFrame()
-df_machopo_almendros = pd.DataFrame()
-df_nenskra_Enguri = pd.DataFrame()
-df_uncompahgre_ridgway = pd.DataFrame()
+    n_pixeles_nieve = ((df >= 40) & (df <= 100)).sum().sum()
+    area_pixel_nieve = abs(snow_cover.rio.resolution()[0] * snow_cover.rio.resolution()[1])
 
-df_datos = pd.DataFrame()
+    return (area_pixel_nieve * n_pixeles_nieve)/1e6
 
 # Lectura de datos
+cuencas = os.listdir(data_path)
 for cuenca in cuencas:
-    try:
+    # Lectura de archivos
+    archivos_hdf = [str(archivo) for archivo in Path(data_path + "/" + cuenca).rglob("*.hdf")]
+    archivos_shp = [str(archivo) for archivo in Path(data_path + "/" + cuenca).glob("*.shp")]
+    area_path = archivos_shp[0]
+    area = gpd.read_file(area_path)
+    if len(archivos_hdf) < 1:
+        print("No se han encontrado archivos hdf en el directorio ", data_path + "/" + cuenca)
+        continue
+    if len(archivos_shp) < 1:
+        print("No se han encontrado archivos shp en el directorio ", data_path + "/" + cuenca)
+        continue
 
-        # Lectura de archivos
-        archivos_hdf = [str(archivo) for archivo in Path(data_path + "/" + cuenca).rglob("*.hdf")]
-        archivos_shp = [str(archivo) for archivo in Path(data_path + "/" + cuenca).glob("*.shp")]
-        area_path = archivos_shp[0]
-        area = gpd.read_file(area_path)
-        if len(archivos_hdf) < 1:
-            print("No se han encontrado archivos hdf en el directorio ", data_path + "/" + cuenca)
-            continue
-        if len(archivos_shp) < 1:
-            print("No se han encontrado archivos shp en el directorio ", data_path + "/" + cuenca)
-            continue
+    fechas = []
+    resultados = []
+    for archivo in archivos_hdf:
 
-        fechas = []
-        resultados = []
-        for archivo in archivos_hdf:
+        # Se busca la fecha en el nombre del fichero hdf
+        coincidencia = re.search(r"_A(\d{4})(\d{3})_", archivo)
+        if coincidencia:
+            # Cambio de formato de fecha
+            fecha = datetime.datetime.strptime(f"{coincidencia.group(1)}-{coincidencia.group(2)}", "%Y-%j").date()
 
-            # Se busca la fecha en el nombre del fichero hdf
-            coincidencia = re.search(r"_A(\d{4})(\d{3})_", archivo)
-            if coincidencia:
-                # Cambio de formato de fecha
-                fecha = datetime.datetime.strptime(f"{coincidencia.group(1)}-{coincidencia.group(2)}", "%Y-%j").date()
+        # snow_cover = open_bands_boundary(archivo, area)
+        snow_cover = rxr.open_rasterio(archivo, masked=True, variable="CGF_NDSI_Snow_Cover").rio.clip(
+            area.geometry.to_list(), crs=area.crs, all_touched=True).squeeze()
 
-            # snow_cover = open_bands_boundary(archivo, area)
-            snow_cover = rxr.open_rasterio(archivo, masked=True, variable="CGF_NDSI_Snow_Cover").rio.clip(
-                area.geometry.to_list(), crs=area.crs, all_touched=True).squeeze()
-            # Reproyecto a WGS84
-            snow_cover = snow_cover.rio.reproject("EPSG:4326")
+        resultados.append({'fecha': fecha, 'area_nieve': calcular_area_nieve(snow_cover, cuenca)})
 
-            snow_mapped = snow_mapping(snow_cover["CGF_NDSI_Snow_Cover"].values)
-            n_ceros = np.sum(snow_mapped == 0)
-            n_unos = np.sum(snow_mapped == 1)
-            resultados.append({'fecha': fecha, 'no_nieve': n_ceros, 'nieve (40-100)': n_unos})
+    df_datos = pd.DataFrame(resultados)
 
-        df_datos = pd.DataFrame(resultados)
-
-        # Ordenar por fecha y cambiar formato
-        df_datos.set_index('fecha', inplace=True)
-        df_datos.index = pd.to_datetime(df_datos.index)
-        df_datos.sort_index(inplace=True)
-        df_datos.index = df_datos.index.strftime('%d/%m/%Y')
-
-        if cuenca == "adda-bornio":
-            df_adda_bornio = df_datos
-        elif cuenca == "genil-dilar":
-            df_genil_dilar = df_datos
-        elif cuenca == "indrawati-melamchi":
-            df_indrawati_almendros = df_datos
-        elif cuenca == "machopo-almendros":
-            df_machopo_almendros = df_datos
-        elif cuenca == "nenskra-Enguri":
-            df_nenskra_Enguri = df_datos
-        elif cuenca == "uncompahgre-ridgway":
-            df_uncompahgre_ridgway = df_datos
-
-    except FileNotFoundError:
-        print(f"El directorio '{data_path}/{cuenca}' no fue encontrado.")
-
-
-# print(df_adda_bornio.to_string())
-# print(df_genil_dilar.to_string())
-# print(df_indrawati_almendros.to_string())
-# print(df_machopo_almendros.to_string())
-# print(df_nenskra_Enguri.to_string())
-# print(df_uncompahgre_ridgway.to_string())
-
-# 25832
-# 25830
-# 32645
-# 32719
-# 32639
-# 32613
+    # Ordenar por fecha
+    df_datos.set_index('fecha', inplace=True)
+    df_datos.sort_index(inplace=True)
+    df_datos.to_csv(cuenca + ".csv")
