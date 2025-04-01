401e1748f8704bb53ff03f8baa306dd7523a8b29
Kike <palavix@gmail.com>
2025-04-01 09:09:48 +0200

    Elimino trasnformacion_datos por procesamiento_datos

--- test_data.py
+++ test_data.py
@@ -1,87 +1,67 @@
-#%%
 import os
-import numpy as np
 import pandas as pd
 import geopandas as gpd
 from pathlib import Path
 import re
 import datetime
 import rioxarray as rxr
+import concurrent.futures
 
-#%%
-external_disk = "D:/"
+external_disk = "E:/"
 data_path = os.path.join(external_disk, "data/", "cuencas/")
 
-# Función para mapear todos los valores entre 40 y 100 (inclusive) a:
-    # 0: no hay nieve
-    # 1: hay nieve
-def snow_mapping(array):
-    nieve = (array >= 40) & (array <= 100)
-    return np.where(nieve, 1, 0)
+def calcular_area_nieve(snow_cover, cuenca):
+    if cuenca == "adda-bornio":
+        snow_cover = snow_cover.rio.reproject("EPSG:25832")
+    elif cuenca == "genil-dilar":
+        snow_cover = snow_cover.rio.reproject("EPSG:25830")
+    elif cuenca == "indrawati-melamchi":
+        snow_cover = snow_cover.rio.reproject("EPSG:32645")
+    elif cuenca == "machopo-almendros":
+        snow_cover = snow_cover.rio.reproject("EPSG:32719")
+    elif cuenca == "nenskra-Enguri":
+        snow_cover = snow_cover.rio.reproject("EPSG:32638")
+    elif cuenca == "uncompahgre-ridgway":
+        snow_cover = snow_cover.rio.reproject("EPSG:32613")
 
+    df = pd.DataFrame(snow_cover["CGF_NDSI_Snow_Cover"])
 
-df_datos = pd.DataFrame()
+    n_pixeles_nieve = ((df >= 40) & (df <= 100)).sum().sum()
+    area_pixel_nieve = abs(snow_cover.rio.resolution()[0] * snow_cover.rio.resolution()[1])
+
+    return (area_pixel_nieve * n_pixeles_nieve)/1e6
+
+def procesar_archivo(cuenca, area, archivo):
+    # print(f"\r{cuenca}: procesando {archivo}...", end="")
+    coincidencia = re.search(r"_A(\d{4})(\d{3})_", archivo)
+    if coincidencia:
+        fecha = datetime.datetime.strptime(f"{coincidencia.group(1)}-{coincidencia.group(2)}", "%Y-%j").date()
+    print(f"[INICIO] {cuenca}: procesando {fecha}")
+
+    snow_cover = rxr.open_rasterio(archivo, masked=True, variable="CGF_NDSI_Snow_Cover").rio.clip(
+        area.geometry.to_list(), crs=area.crs, all_touched=False).squeeze()
+    print(f"[FIN] {cuenca}: procesado {fecha}")
+    return {'fecha': fecha, 'area_nieve': calcular_area_nieve(snow_cover, cuenca)}
+
+def procesar_cuenca(cuenca):
+    archivos_hdf = [str(archivo) for archivo in Path(data_path + "/" + cuenca).rglob("*.hdf")]
+    archivos_hdf = archivos_hdf[:30]
+    archivos_shp = [str(archivo) for archivo in Path(data_path + "/" + cuenca).glob("*.shp")]
+    area_path = archivos_shp[0]
+    area = gpd.read_file(area_path)
+
+    resultados = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(procesar_archivo, cuenca, area, archivo) for archivo in archivos_hdf]
+        for future in concurrent.futures.as_completed(futures):
+            resultados.append(future.result())
+
+    df_datos = pd.DataFrame(resultados)
+    df_datos.set_index('fecha', inplace=True)
+    df_datos.sort_index(inplace=True)
+    # df_datos.to_csv(f"csv/{cuenca}.csv")
+    print(f"\nCuenca {cuenca} procesada.")
+
 cuencas = os.listdir(data_path)
-
-#%%
-# Lectura de datos
-for cuenca in ['adda-bornio']:
-    try:
-
-        archivos_hdf = [str(archivo) for archivo in Path(data_path + "/" + cuenca).rglob("*.hdf")]
-        archivos_hdf = archivos_hdf[:50]
-        archivos_shp = [str(archivo) for archivo in Path(data_path + "/" + cuenca).glob("*.shp")]
-        area_path = archivos_shp[0]
-        area = gpd.read_file(area_path)
-
-        if len(archivos_hdf) < 1:
-            print("No se han encontrado archivos hdf en el directorio ", data_path + "/" + cuenca)
-            continue
-        if len(archivos_shp) < 1:
-            print("No se han encontrado archivos shp en el directorio ", data_path + "/" + cuenca)
-            continue
-
-        fechas = []
-        resultados = []
-        for archivo in archivos_hdf:
-
-            # Se busca la fecha en el nombre del fichero hdf
-            coincidencia = re.search(r"_A(\d{4})(\d{3})_", archivo)
-            if coincidencia:
-                # Cambio de formato de fecha
-                fecha = datetime.datetime.strptime(f"{coincidencia.group(1)}-{coincidencia.group(2)}", "%Y-%j").date().strftime("%d/%m/%Y")
-
-                # snow_cover = open_bands_boundary(archivo, area)
-                snow_cover = rxr.open_rasterio(archivo, masked=True, variable="CGF_NDSI_Snow_Cover").rio.clip(
-                    area.geometry.to_list(), crs=area.crs, all_touched=False).squeeze()
-                snow_cover = snow_cover.rio.reproject("EPSG:4326")
-
-                # Imprimo la resolución espacial desde los metadatos del dataarray
-                resolution = snow_cover.rio.resolution()
-                print(f"Resolución X: {resolution[0]}")
-                print(f"Resolución Y: {resolution[1]}")
-                print(f"CRS: {snow_cover.rio.crs}")
-
-                snow_mapped = snow_mapping(snow_cover["CGF_NDSI_Snow_Cover"])
-                n_ceros = np.sum(snow_mapped == 0)
-                n_unos = np.sum(snow_mapped == 1)
-                resultados.append({'fecha': fecha, 'no_nieve': n_ceros, 'nieve (40-100)': n_unos})
-
-        df = pd.DataFrame(snow_cover["CGF_NDSI_Snow_Cover"])
-        print("Numero de valores no null(255) de ", cuenca, ": ", df.count().sum())
-
-
-        df_datos = pd.DataFrame(resultados)
-
-        # Ordenar por fecha y cambiar formato
-        df_datos.set_index('fecha', inplace=True)
-        df_datos.index = pd.to_datetime(df_datos.index, format='%d/%m/%Y')
-        df_datos.sort_index(inplace=True)
-        df_datos.index = df_datos.index.strftime('%d/%m/%Y')
-
-    except FileNotFoundError:
-        print(f"El directorio '{data_path}/{cuenca}' no fue encontrado.")
-
-
-#%%
-df_datos.to_csv("adda-bornio50.csv")
+for cuenca in cuencas:
+    procesar_cuenca(cuenca)
