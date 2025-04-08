9f6acfb6fa4a300bb425db840f959bac2a2d833e
Kike Merino <palavix@gmail.com>
2025-04-08 10:22:49 +0200

    Completar preprocesamiento_datos.py

--- procesamiento_datos.py
+++ procesamiento_datos.py
@@ -1,8 +1,9 @@
 #%%
 import pandas as pd
-
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 #%%
-csv_path = "E:/data/csv/"
+csv_path = "D:/data/csv/"
 cuencas = ['adda-bornio','genil-dilar','indrawati-melamchi','machopo-almendros','nenskra-Enguri','uncompahgre-ridgway']
 
 area_ab = pd.read_csv(csv_path + "areas/" + cuencas[0] + ".csv")
@@ -26,6 +27,32 @@ variable_ur = pd.read_csv(csv_path + "series_agregadas/" + cuencas[5] + ".csv")
 
 #%%
 def procesar_cuenca(areas, series):
+    """
+    Combina datos de áreas y series temporales de una cuenca hidrográfica,
+    calcula los días transcurridos desde la última precipitación y devuelve
+    un DataFrame con la información procesada.
+
+    Args:
+        areas (pd.DataFrame): DataFrame con información de áreas,
+                                 debe contener una columna 'fecha' para la unión.
+        series (pd.DataFrame): DataFrame con series temporales (temperatura,
+                                  precipitación, etc.), debe contener una columna
+                                  'fecha' para la unión.
+
+    Returns:
+        pd.DataFrame: DataFrame resultante de la combinación y el cálculo,
+                      con las siguientes columnas:
+                      - 'fecha': Fecha de la observación (tipo datetime).
+                      - 'dia_sen': Dia normalizado con la función seno.
+                      - 'temperatura': Temperatura registrada.
+                      - 'precipitacion': Cantidad de precipitación registrada.
+                      - 'precipitacion_bool': Variable booleana indicando si hubo
+                                              precipitación (1 para sí,
+                                              0 para no).
+                      - 'area_nieve': Área cubierta por nieve.
+                      - 'dias_sin_precip': Número de días consecutivos sin
+                                           precipitación hasta la fecha actual.
+    """
     df = pd.merge(series, areas, how='inner', left_on='fecha', right_on='fecha')
     columns = ['fecha','dia_sen','temperatura','precipitacion','precipitacion_bool','area_nieve']
     df.columns = columns
@@ -36,7 +63,7 @@ def procesar_cuenca(areas, series):
 
     for index, row in df.iterrows():
         if row['precipitacion_bool'] == 1:
-            dias_transcurridos = 0  # reinicia el contador si ha llovido 
+            dias_transcurridos = 0  # reinicia el contador si ha llovido
         else:
             dias_transcurridos += 1
 
@@ -47,35 +74,37 @@ def procesar_cuenca(areas, series):
 #adda-bornio
 adda = procesar_cuenca(area_ab, variable_ab)
 adda.describe()
-
 #%%
 #genil-dilar
 genil = procesar_cuenca(area_gd, variable_gd)
 genil.describe()
-
 #%%
 #indrawati-melamchi
 indrawati = procesar_cuenca(area_im, variable_im)
 indrawati.describe()
-
 #%%
 # machopo-almendros
 machopo = procesar_cuenca(area_ma, variable_ma)
 machopo.describe()
-
 #%%
 #nenskra-enguri
 nenskra = procesar_cuenca(area_ne, variable_ne)
 nenskra.describe()
-
 #%%
 #uncompahgre-ridgway
 uncompahgre = procesar_cuenca(area_ur, variable_ur)
 uncompahgre.describe()
 
 #%%
-corr = adda.corr(numeric_only=True)
-corr.style.background_gradient(cmap="coolwarm")
+# MixMax Scaler y correlacion de adda-bornio
+adda_norm = adda.copy()
+MinMax = MinMaxScaler()
+adda_norm[['dia_sen', 'temperatura', 'precipitacion', 'dias_sin_precip']] = MinMax.fit_transform(adda[['dia_sen', 'temperatura', 'precipitacion', 'dias_sin_precip']])
+corr_adda = adda_norm.corr(numeric_only=True)
+corr_adda.style.background_gradient(cmap="coolwarm")
+
+#%%
+adda_norm.to_csv(".")
 
 #%%
 corr = genil.corr(numeric_only=True)
